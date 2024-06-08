0e87a68bd5b2c326f867d67debd2eb021b438009
Robin Roeland <rroeland03@gmail.com>
2024-06-08 19:00:30 +0200

    Basic article view

--- src/main/java/be/gazette/gazette_website/ArticleCRUDRepo.java
+++ src/main/java/be/gazette/gazette_website/ArticleCRUDRepo.java
@@ -1,7 +1,6 @@
 package be.gazette.gazette_website;
 
 import org.springframework.data.repository.CrudRepository;
-import java.sql.Date;
 import java.util.List;
 
 public interface ArticleCRUDRepo extends CrudRepository<Article, Long> {
